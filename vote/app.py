b7b42293440b5dfaebdbde6662505db6e11ac795
aggviolinist <mulandimaei76@gmail.com>
2025-11-23 18:00:18 +0300

    app update

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "ArgoCD")
-option_b = os.getenv('OPTION_B', "Ansible")
+option_a = os.getenv('OPTION_A', "Potato")
+option_b = os.getenv('OPTION_B', "Orange")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
